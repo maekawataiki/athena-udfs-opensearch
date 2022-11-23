7772518a602d457a8b84713c4844a561646eb8d3
Taiki Maekawa <tmae@amazon.com>
2022-11-23 15:41:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Amazon Athena UDF Connector OpenSearch Full Text Search Example
+
+This is example Athena UDF Connector to perform full text search on OpenSearch.
+
+This repo has is extended from [ahtena-udfs](https://github.com/awslabs/aws-athena-query-federation/blob/master/athena-udfs/src/main/java/com/amazonaws/athena/connectors/udfs/AthenaUDFHandler.java) repo.
+
+## Added UDFs
+
+1. "search": Search OpenSearch
+
+Example query:
+
+```
+USING EXTERNAL FUNCTION search(
+	host VARCHAR,
+	region VARCHAR,
+	index VARCHAR,
+	keyword VARCHAR,
+	limit INTEGER
+) RETURNS ARRAY <VARCHAR> LAMBDA 'customudf'
+SELECT search(
+		'xxx.us-west-2.es.amazonaws.com',
+		'us-west-2',
+		'wikipedia',
+		'keyword',
+		20
+	);
+```
+
+This would return result [aaa, bbb, ccc, ddd, ...].
+
+### Repositories for AWS built UDFs
+
+1. https://github.com/aws-samples/aws-athena-udfs-textanalytics
+
+## Deploying The Connector
+
+To use this connector in your queries, navigate to AWS Serverless Application Repository and deploy a pre-built version of this connector. Alternatively, you can build and deploy this connector from source follow the below steps or use the more detailed tutorial in the athena-example module:
+
+1. From the athena-federation-sdk dir, run `mvn clean install` if you haven't already.
+2. From the athena-udfs dir, run `mvn clean install`.
+3. From the athena-udfs dir, run  `../tools/publish.sh S3_BUCKET_NAME athena-udfs REGION` to publish the connector to your private AWS Serverless Application Repository. The S3_BUCKET in the command is where a copy of the connector's code will be stored for Serverless Application Repository to retrieve it. This will allow users with permission to do so, the ability to deploy instances of the connector via 1-Click form. Then navigate to [Serverless Application Repository](https://aws.amazon.com/serverless/serverlessrepo)
+4. Try using your UDF(s) in a query.
+
+## License
+
+This project is licensed under the Apache-2.0 License.
---
+++ src/main/java/com/amazonaws/athena/connectors/udfs/AthenaUDFHandler.java
@@ -0,0 +1,296 @@
+/*-
+ * #%L
+ * athena-udfs
+ * %%
+ * Copyright (C) 2019 Amazon Web Services
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.amazonaws.athena.connectors.udfs;
+
+import com.amazonaws.athena.connector.lambda.handlers.UserDefinedFunctionHandler;
+import com.amazonaws.athena.connector.lambda.security.CachableSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClient;
+import com.google.common.annotations.VisibleForTesting;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch.core.SearchResponse;
+import org.opensearch.client.transport.aws.AwsSdk2Transport;
+import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+public class AthenaUDFHandler
+        extends UserDefinedFunctionHandler
+{
+    public static class IndexData
+    {
+        private String title;
+        private String content;
+
+        public IndexData()
+        {
+
+        }
+    
+        public IndexData(String title, String content)
+        {
+        this.title = title;
+        this.content = content;
+        }
+    
+        public String getTitle()
+        {
+        return title;
+        }
+    
+        public void setTitle(String title)
+        {
+        this.title = title;
+        }
+    
+        public String getContent()
+        {
+        return content;
+        }
+    
+        public void setContent(String content)
+        {
+        this.content = content;
+        }
+    
+        @Override
+        public String toString()
+        {
+        return String.format("IndexData{title='%s', content='%s'}", title, content);
+        }
+    }
+
+    private static final String SOURCE_TYPE = "athena_common_udfs";
+
+    private final CachableSecretsManager cachableSecretsManager;
+
+    public AthenaUDFHandler()
+    {
+        this(new CachableSecretsManager(AWSSecretsManagerClient.builder().build()));
+    }
+
+    @VisibleForTesting
+    AthenaUDFHandler(CachableSecretsManager cachableSecretsManager)
+    {
+        super(SOURCE_TYPE);
+        this.cachableSecretsManager = cachableSecretsManager;
+    }
+
+    /**
+     * Search OpenSearch
+     * 
+     * @param host    domain of OpenSearch
+     * @param region  region of OpenSearch
+     * @param index   index of OpenSearch
+     * @param keyword keyword for search
+     * @param limit   number of search results
+     * @return List of Object
+     */
+    public List<String> search(String host, String region, String index, String keyword, Integer limit)
+    {
+        SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+        try {
+            OpenSearchClient client = new OpenSearchClient(
+                    new AwsSdk2Transport(
+                            httpClient,
+                            host,
+                            Region.of(region),
+                            AwsSdk2TransportOptions.builder().build()));
+
+            SearchResponse<IndexData> searchResponse = client.search(s -> s
+                .index(index)
+                .query(q -> q
+                    .multiMatch(mm -> mm.fields("title^2", "content").query(keyword))
+                ), 
+                IndexData.class);
+            List<String> result = new ArrayList<>();
+            searchResponse.hits().hits().stream().forEach(hit -> result.add(hit.source().getTitle()));
+            return result;
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Failed to query OpenSearch", e);
+        }
+        finally {
+            httpClient.close();
+        }
+    }
+
+    /**
+     * Compresses a valid UTF-8 String using the zlib compression library.
+     * Encodes bytes with Base64 encoding scheme.
+     *
+     * @param input the String to be compressed
+     * @return the compressed String
+     */
+    public String compress(String input)
+    {
+        byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
+
+        // create compressor
+        Deflater compressor = new Deflater();
+        compressor.setInput(inputBytes);
+        compressor.finish();
+
+        // compress bytes to output stream
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(inputBytes.length);
+        while (!compressor.finished()) {
+            int bytes = compressor.deflate(buffer);
+            byteArrayOutputStream.write(buffer, 0, bytes);
+        }
+
+        try {
+            byteArrayOutputStream.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to close ByteArrayOutputStream", e);
+        }
+
+        // return encoded string
+        byte[] compressedBytes = byteArrayOutputStream.toByteArray();
+        return Base64.getEncoder().encodeToString(compressedBytes);
+    }
+
+    /**
+     * Decompresses a valid String that has been compressed using the zlib compression library.
+     * Decodes bytes with Base64 decoding scheme.
+     *
+     * @param input the String to be decompressed
+     * @return the decompressed String
+     */
+    public String decompress(String input)
+    {
+        byte[] inputBytes = Base64.getDecoder().decode((input));
+
+        // create decompressor
+        Inflater decompressor = new Inflater();
+        decompressor.setInput(inputBytes, 0, inputBytes.length);
+
+        // decompress bytes to output stream
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(inputBytes.length);
+        try {
+            while (!decompressor.finished()) {
+                int bytes = decompressor.inflate(buffer);
+                if (bytes == 0 && decompressor.needsInput()) {
+                    throw new DataFormatException("Input is truncated");
+                }
+                byteArrayOutputStream.write(buffer, 0, bytes);
+            }
+        }
+        catch (DataFormatException e) {
+            throw new RuntimeException("Failed to decompress string", e);
+        }
+
+        try {
+            byteArrayOutputStream.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to close ByteArrayOutputStream", e);
+        }
+
+        // return decoded string
+        byte[] decompressedBytes = byteArrayOutputStream.toByteArray();
+        return new String(decompressedBytes, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * This method decrypts the ciphertext with a data key stored AWS Secret Manager. Before using this function, create
+     * a secret in AWS Secret Manager. Do a base64 encode to your data key and convert it to string. Store it as
+     * _PLAINTEXT_ in the secret (do not include any quotes, brackets, etc). Also make sure to use DefaultEncryptionKey
+     * as the KMS key. Otherwise you would need to update athena-udfs.yaml to allow access to your KMS key.
+     *
+     * @param ciphertext
+     * @param secretName
+     * @return plaintext
+     */
+    public String decrypt(String ciphertext, String secretName)
+    {
+        String secretString = cachableSecretsManager.getSecret(secretName);
+        byte[] plaintextKey = Base64.getDecoder().decode(secretString);
+
+        try {
+            Cipher cipher = getCipher(Cipher.DECRYPT_MODE, plaintextKey);
+            byte[] encryptedContent = Base64.getDecoder().decode(ciphertext.getBytes());
+            byte[] plainTextBytes = cipher.doFinal(encryptedContent);
+            return new String(plainTextBytes);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * This method encrypts the plaintext with a data key stored AWS Secret Manager. Before using this function, create
+     * a secret in AWS Secret Manager. Do a base64 encode to your data key and convert it to string. Store it as
+     * _PLAINTEXT_ in the secret (do not include any quotes, brackets, etc). Also make sure to use DefaultEncryptionKey
+     * as the KMS key. Otherwise you would need to update athena-udfs.yaml to allow access to your KMS key.
+     *
+     * @param plaintext
+     * @param secretName
+     * @return ciphertext
+     */
+    public String encrypt(String plaintext, String secretName)
+    {
+        String secretString = cachableSecretsManager.getSecret(secretName);
+        byte[] plaintextKey = Base64.getDecoder().decode(secretString);
+
+        try {
+            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, plaintextKey);
+            byte[] encryptedContent = cipher.doFinal(plaintext.getBytes());
+            byte[] encodedContent = Base64.getEncoder().encode(encryptedContent);
+            return new String(encodedContent);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Cipher getCipher(int cipherMode, byte[] plainTextDataKey)
+    {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            SecretKeySpec skeySpec = new SecretKeySpec(plainTextDataKey, "AES");
+            cipher.init(cipherMode, skeySpec);
+            return cipher;
+        }
+        catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
